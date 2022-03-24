6e084f4bc52080268c43625b0215198119d0af26
Mynul Islam <sk470004@gmail.com>
2022-03-24 22:58:35 +0600

    adding pure .docx file and everyting ok

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const { Document, Packer, Paragraph, TextRun } = require("docx");
+const PizZip = require("pizzip");
+const Docxtemplater = require("docxtemplater");
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const WordExtractor = require("word-extractor");
-const fsPromises = require('fs').promises;
 const readFileName = 'template.doc';
 const writeFileName = 'proposal.doc';
 const fs = require('fs');
@@ -31,57 +32,98 @@ async function run() {
         const usersCollection = database.collection('users');
 
         // GET API
+
         app.post('/downloadPersonal', async (req, res) => {
+
             const loginData = req.body;
             const { projectBased, projectTime } = loginData;
-            // console.log(projectBased,projectTime,loginData)
-            const extractor = new WordExtractor();
-            const extracted = extractor.extract(readFileName);
-            let text = ''
-            await extracted.then(function (doc) { text = doc.getBody(); });
-            // console.log(text)
-            // replace the things which needed
-            let replacedName = await text.replace(/projectName/g, projectBased)
-            let replacedMonths = await replacedName.replace(/projectMonth/g, projectTime)
-            // split the array for difference the heder and text
-            const myArray = await replacedMonths.split("\n\n\n");
-            const userHeder = await myArray[0]
-            const userText = await myArray[1]
-            // make  sting for file
-            const doc = new Document({
-                sections: [{
-                    properties: {},
-                    children: [
-                        new Paragraph({
-                            children: [
-                                new TextRun({
-                                    text: userHeder,
-                                    bold: true,
-                                    size: 50,
-                                    color: '#00B050',
-                                }),
+            const fs = require("fs");
+            const path = require("path");
 
-                            ],
-                        }),
-                        new Paragraph({}),
-                        new Paragraph({}),
-                        new Paragraph({}),
-                        new Paragraph({
-                            children: [
-                                new TextRun(userText),
+            // Load the docx file as binary content
+            const content = fs.readFileSync(
+                path.resolve(__dirname, "template.docx"),
+                "binary"
+            );
 
-                            ],
-                        }),
-                    ],
-                }],
-            });
-            // now write the string on file
-            await Packer.toBuffer(doc).then((buffer) => {
-                fs.writeFileSync(writeFileName, buffer);
+            const zip = new PizZip(content);
+
+            const doc = new Docxtemplater(zip, {
+                paragraphLoop: true,
+                linebreaks: true,
             });
 
+            // Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
+            doc.render({
+                projectName: projectBased,
+                projectMonth: projectTime,
+
+            });
+
+            const buf = doc.getZip().generate({
+                type: "nodebuffer",
+                // compression: DEFLATE adds a compression step.
+                // For a 50MB output document, expect 500ms additional CPU time
+                compression: "DEFLATE",
+            });
+
+            // buf is a nodejs Buffer, you can either write it to a file or res.send it with express for example.
+            fs.writeFileSync(path.resolve(__dirname, "proposal.doc"), buf);
             res.download('./proposal.doc')
         })
+
+
+        // app.post('/downloadPersonal', async (req, res) => {
+        //     const loginData = req.body;
+        //     const { projectBased, projectTime } = loginData;
+        //     // console.log(projectBased,projectTime,loginData)
+        //     const extractor = new WordExtractor();
+        //     const extracted = extractor.extract(readFileName);
+        //     let text = ''
+        //     await extracted.then(function (doc) { text = doc.getBody(); });
+        //     // console.log(text)
+        //     // replace the things which needed
+        //     let replacedName = await text.replace(/projectName/g, projectBased)
+        //     let replacedMonths = await replacedName.replace(/projectMonth/g, projectTime)
+        //     // split the array for difference the heder and text
+        //     const myArray = await replacedMonths.split("\n\n\n");
+        //     const userHeder = await myArray[0]
+        //     const userText = await myArray[1]
+        //     // make  sting for file
+        //     const doc = new Document({
+        //         sections: [{
+        //             properties: {},
+        //             children: [
+        //                 new Paragraph({
+        //                     children: [
+        //                         new TextRun({
+        //                             text: userHeder,
+        //                             bold: true,
+        //                             size: 50,
+        //                             color: '#00B050',
+        //                         }),
+
+        //                     ],
+        //                 }),
+        //                 new Paragraph({}),
+        //                 new Paragraph({}),
+        //                 new Paragraph({}),
+        //                 new Paragraph({
+        //                     children: [
+        //                         new TextRun(userText),
+
+        //                     ],
+        //                 }),
+        //             ],
+        //         }],
+        //     });
+        //     // now write the string on file
+        //     await Packer.toBuffer(doc).then((buffer) => {
+        //         fs.writeFileSync(writeFileName, buffer);
+        //     });
+
+        //     res.download('./proposal.doc')
+        // })
         // POST API 
         // insert one
         app.post('/user', async (req, res) => {
